a6eacbd42a761c3f5405472817a5b86ef794c39a
phothinmg <phothinmg@gmail.com>
2023-12-21 17:58:45 +0630

    modified:   README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-## Burmese Date 
+<main style="font-family:monospace; font-weight: bold;">
+    <h2>Burmese Date </h2>
+    <hr />
+    <p> A simple project of Burmese Cleandar Data.</p>
+    <pre>
+      <code>npm i burmese</code>
+    </pre>
+</main>
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { DateTime,burmeseDate } from "./buemese-date/burmeseDate.js";
 import data from './database/tz.js';
-namespace BurmeseDate{
+namespace BurmeseCleandar{
     type trs ={
         text: string;
         lang: number
       }
-      export const translate = ({text,lang}: trs) =>{
+    const translate = ({text,lang}: trs) =>{
         const language  = [
             [
                 " ",
@@ -758,32 +758,32 @@ namespace BurmeseDate{
         return text
       
       };
-      export function mmdate(date:string,lang:number){
+      export function GetCleandarData(date:string,lang:number){
         const y = parseInt(date.split('-')[0]);
         const m = parseInt(date.split('-')[1]);
         const d = parseInt(date.split('-')[2]);
-        const jdn = DateTime.w2j(y,m,d);
-        const mdt = new burmeseDate(jdn, 6.5);
-        const ssy = translate({text : mdt.ToMString('&YYYY'), lang: lang});
-        const mmy = translate({text:mdt.ToMString('&yyyy'),lang:lang});
-        const mmm = translate({text:mdt.ToMString('&M'),lang:lang});
-        const mmd = translate({text:mdt.ToMString('&f'),lang:lang});
-        const mwd = translate({text:mdt.ToString('%W '),lang:lang});
-        const mp = translate({text:mdt.ToMString('&P'),lang:lang});
-        const yyz = translate({text:mdt.yatyaza,lang:lang});
-        const ptd = translate({text:mdt.pyathada,lang:lang});
-        const dgh = translate({text:mdt.nagahle,lang:lang});
-        const sbat = translate({text:mdt.sabbath,lang:lang});
-        const mmlen = translate({text:mdt.mmlen.toString(), lang:lang});
-        const MY = burmeseDate.j2m(jdn).my;
-        // const mmyt = ["common","little watat","big watat"][mdt.mf]
-        // const myt = translate({text:mmyt,lang:lang});
+        const julianDayNumber = DateTime.w2j(y,m,d);
+        const mdt = new burmeseDate(julianDayNumber, 6.5);
+        const sasanaYear = translate({text : mdt.ToMString('&YYYY'), lang: lang});
+        const burmeseYear = translate({text:mdt.ToMString('&yyyy'),lang:lang});
+        const burmeseMonth = translate({text:mdt.ToMString('&M'),lang:lang});
+        const burmeseDay = translate({text:mdt.ToMString('&f'),lang:lang});
+        const burmeseWeekDay = translate({text:mdt.ToString('%W '),lang:lang});
+        const lunarPhase = translate({text:mdt.ToMString('&P'),lang:lang});
+        const yatyarzar = translate({text:mdt.yatyaza,lang:lang});
+        const pyatthadar = translate({text:mdt.pyathada,lang:lang});
+        const dragonHead = translate({text:mdt.nagahle,lang:lang});
+        const sabbath = translate({text:mdt.sabbath,lang:lang});
+        const lengthOfBurmeseMonth = translate({text:mdt.mmlen.toString(), lang:lang});
+        const BURMESE_YEAR_IN_NUMBER = burmeseDate.j2m(julianDayNumber).my;
+        const mmyt = ["common","little watat","big watat"][mdt.myt];
+        const warHtutType = translate({text:mmyt,lang:lang});
         const h = mdt.holidays;
-        let hd1: string;
+        let publicHolidays: string;
         if(h.length === 0){
-            hd1 = ''
+          publicHolidays = ''
         } else {
-            hd1 = h.reduce(function(acc: string, currentValue: string) {
+          publicHolidays = h.reduce(function(acc: string, currentValue: string) {
                 return translate({text:acc,lang:lang}) + ',' + translate({text:currentValue,lang:lang});
             });
         }
@@ -796,17 +796,22 @@ namespace BurmeseDate{
                 return translate({text:acc,lang:lang}) + ',' + translate({text:currentValue,lang:lang});
            });
         }
-        const myn = translate({text:mdt.my_name,lang: lang});
-        const mhb = translate({text:mdt.mahabote,lang: lang});
-        const nk = translate({text:mdt.nakhat,lang: lang});
-        let ast: string[] = [];
+        const nameOfBurmeseYear = translate({text:mdt.my_name,lang: lang});
+        const maharbote = translate({text:mdt.mahabote,lang: lang});
+        const nakhat = translate({text:mdt.nakhat,lang: lang});
+        let astroDays: string[] = [];
         mdt.astro.forEach((item:string)=>{
             const a: string = translate({text: item, lang: lang});
-            ast.push(a);
+            astroDays.push(a);
         });
     
         
-        return {ssy, mmy, myn, mmm, mp, mmd, yyz,ptd, dgh, sbat, hd1, hd2, mhb, nk,ast, mwd, MY, mmlen}
+        return {
+          julianDayNumber, astroDays, publicHolidays, sasanaYear, burmeseYear,
+          burmeseMonth, burmeseDay, burmeseWeekDay, lunarPhase, yatyarzar,
+          pyatthadar, dragonHead, sabbath, lengthOfBurmeseMonth, BURMESE_YEAR_IN_NUMBER,
+          warHtutType, nameOfBurmeseYear, maharbote, nakhat 
+        }
     
        
     
@@ -824,40 +829,50 @@ namespace BurmeseDate{
         return {ja:ja,jk:jk,da:Math.round(ja),dk:Math.round(jk)};
     };
     
-    export function getThingyan (my: number){
+    export function MaharThingyan (my: number){
         const 
-        to = my + 1,
-        from = my,
+        YearTo = my + 1,
+        YearFrom = my,
         emName=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-        ppp = thingyanTime(to),
+        ppp = thingyanTime(YearTo),
         att = burmeseDate.j2w(ppp.ja),
-        atatTime = att.y + '-' + emName[att.m-1] + '-' + att.d + ' ' + att.h + ':' + att.n + ':' + Math.floor(att.s),
+        AtatTime = att.y + '-' + emName[att.m-1] + '-' + att.d + ' ' + att.h + ':' + att.n + ':' + Math.floor(att.s),
         akt = burmeseDate.j2w(ppp.jk),
-        akyaTime = `${akt.y}-${emName[akt.m-1]}-${akt.d} ${akt.h}:${akt.n}:${Math.floor(akt.s)}`,
+        AkyaTime = `${akt.y}-${emName[akt.m-1]}-${akt.d} ${akt.h}:${akt.n}:${Math.floor(akt.s)}`,
         akd = burmeseDate.j2w(ppp.dk),
         akyod = burmeseDate.j2w(ppp.dk - 1),
-        akyoDay = `${akyod.y}-${emName[akyod.m-1]}-${akyod.d}`,
-        akyaDay = `${akd.y}-${emName[akd.m-1]}-${akd.d}`,
+        AkyoDay = `${akyod.y}-${emName[akyod.m-1]}-${akyod.d}`,
+        AkyaDay = `${akd.y}-${emName[akd.m-1]}-${akd.d}`,
         akyatd = burmeseDate.j2w(ppp.dk +1),
-        akyatDay = `${akyatd.y}-${emName[akyatd.m-1]}-${akyatd.d}`,
+        AkyatDay = `${akyatd.y}-${emName[akyatd.m-1]}-${akyatd.d}`,
         atd = burmeseDate.j2w(ppp.da),
-        atatDay = `${atd.y}-${emName[atd.m-1]}-${atd.d}`,
+        AtatDay = `${atd.y}-${emName[atd.m-1]}-${atd.d}`,
         nyd = burmeseDate.j2w(ppp.da + 1),
-        nyDay = `${nyd.y}-${emName[nyd.m-1]}-${nyd.d}`;
-        let sakd : string;
+        NewYearDay = `${nyd.y}-${emName[nyd.m-1]}-${nyd.d}`;
+        let AkyatDay2 : string;
         if(ppp.da-ppp.dk>2){
             const sakdd = burmeseDate.j2w(ppp.da -1);
-            sakd = `${sakdd.y}-${emName[sakdd.m-1]}-${sakdd.d}`;
+            AkyatDay2 = `${sakdd.y}-${emName[sakdd.m-1]}-${sakdd.d}`;
         }else{
-            sakd = ''
+            AkyatDay2 = ''
         }
-    
-        return {from, to, atatTime, akyaTime, akyoDay, akyaDay, akyatDay, sakd, atatDay, nyDay}
+        return {
+          YearTo,
+          YearFrom,
+          AtatTime,
+          AkyaTime,
+          AkyoDay,
+          AkyaDay,
+          AkyatDay,
+          AkyatDay2,
+          AtatDay,
+          NewYearDay
+        }
     };
     const dayOfYear = (date: any)=> {
         return Math.floor((date.getTime() - new Date(date.getFullYear(), 0, 0).getTime()) / 1000 / 60 / 60 / 24);
     };
-    export function worldTime(timeZoneName : string){
+    export function WorldTimeZone(timeZoneName : string){
         const continent= timeZoneName.split('/')[0];
         const dataTofind: any = data.find(item => item.group.includes(timeZoneName) && item.name === timeZoneName);//
         const rawOffset = - (dataTofind.rawOffsetInMinutes); // Time zone offset in minutes
@@ -891,4 +906,5 @@ namespace BurmeseDate{
       
 }
 
-export default BurmeseDate;
+export default BurmeseCleandar;
+
